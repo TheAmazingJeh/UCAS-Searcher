6e0a202619ce2baa7de0ec8e486c1cb669927b7c
theamazingjeh <joehbooth2004@gmail.com>
2023-11-24 10:51:44 +0000

    Add UCAS_handler class and main function

---
+++ UCAS Search.py
@@ -0,0 +1,12 @@
+class UCAS_handler:
+    def __init__(self):
+        self.uniInput = ""
+        self.uniName = ""
+        self.subjectInput = ""
+        self.subjectName = ""
+
+def main():
+    # Create UCAS_handler object
+    handler = UCAS_handler()
+if __name__ == "__main__":
+    main()
