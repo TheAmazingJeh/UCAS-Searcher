410fb683e597f83285274b32be96dd0d2966b1d5
theamazingjeh <joehbooth2004@gmail.com>
2023-11-24 10:53:56 +0000

    Add select_from_list method to UCAS_handler class

--- UCAS Search.py
+++ UCAS Search.py
@@ -20,6 +20,36 @@ class UCAS_handler:
 
         data = json.dumps({"searchTerm": searchTerm})
         return dict(rPost(url, headers=headers, data=data).json())
+
+    def select_from_list(self, optionList):
+        """
+        Allows the user to select an option from a list of options using numbers.
+
+        Returns the selected option.
+        """
+        # If there are no options, return False
+        if optionList == []:
+            print("No Match")
+            return False
+        
+        # If there is only one option, return that option
+        if len(optionList) == 1:
+            print(f"Selected {optionList[0]}")
+            return optionList[0]
+        
+        # If there are multiple options, ask the user to select one
+        i=1
+        for item in optionList: print(f"{i:2} - {item}"); i+=1
+        course = input(f"Please select (1-{len(optionList)}): ")
+
+        # If the user didn't select a valid option, return False
+        if not course.isdigit() or int(course) > len(optionList) or int(course) < 1:
+            print("Invalid Selection")
+            return False
+        
+        # Return the selected option
+        return optionList[int(course)-1]
+
 def main():
     # Create UCAS_handler object
     handler = UCAS_handler()
