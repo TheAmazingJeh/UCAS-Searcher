a8bb6b289724036d3f708d1fabb516614f680259
theamazingjeh <joehbooth2004@gmail.com>
2023-11-24 10:52:33 +0000

    Add UCAS search functionality

--- UCAS Search.py
+++ UCAS Search.py
@@ -1,3 +1,5 @@
+import json, sys
+from requests import post as rPost
 class UCAS_handler:
     def __init__(self):
         self.uniInput = ""
@@ -5,6 +7,19 @@ class UCAS_handler:
         self.subjectInput = ""
         self.subjectName = ""
 
+    def search_ucas(self, searchTerm):
+        """
+        Searches UCAS for the input.
+
+        Will return a dictionary with the keys `courseTitles`, `locations`, `providers` and `subjects`
+        """
+
+        url = "https://services.ucas.com/search/api/v2/autocomplete"
+        headers = CaseInsensitiveDict()
+        headers["Content-Type"] = "application/json"
+
+        data = json.dumps({"searchTerm": searchTerm})
+        return dict(rPost(url, headers=headers, data=data).json())
 def main():
     # Create UCAS_handler object
     handler = UCAS_handler()
